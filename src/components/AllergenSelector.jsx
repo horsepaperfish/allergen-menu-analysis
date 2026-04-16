eff4d6d98259485a45fba840cb219af81df67bf0
Ma Zhiyu <zhiyuma2006@gmail.com>
2026-04-15 21:34:21 -0400

    Fix Colours

--- src/components/AllergenSelector.jsx
+++ src/components/AllergenSelector.jsx
@@ -71,12 +71,6 @@ function AllergenSelector({ onContinue }) {
   return (
     <div className="allergen-selector">
       <div className="selector-header">
-        <div className="logo-circle">
-          <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-            <path d="M9 12l2 2 4-4" />
-          </svg>
-        </div>
         <h1>Allergen Menu Analyzer</h1>
         <p className="selector-subtitle">Select your allergies, then scan any menu</p>
       </div>
